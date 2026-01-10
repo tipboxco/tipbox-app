de6e3a8a918e92e04b4d3d9c9c6ba845e044ddb9
Ömer Faruk DEMİRAL <omer.frk027@gmail.com>
2026-01-10 22:11:04 +0300

    fix: Replace ozan.png references with DEFAULT_USER_AVATAR in Trust_TrusterListScreen
    - Update all avatar references to use DEFAULT_USER_AVATAR constant
    - Add DEFAULT_USER_AVATAR import from utils
    - Fixes bundling error for missing ozan.png asset

--- src/features/profile/screens/Trust_TrusterListScreen.tsx
+++ src/features/profile/screens/Trust_TrusterListScreen.tsx
@@ -23,7 +23,7 @@ import { SuggestionCard } from '../components/SuggestionCard';
 import { useGlobalBottomSheet } from '@/src/hooks/useGlobalBottomSheet';
 import { useTrustList, useTrusterList, useUserProfile } from '../api/hooks';
 import { ProfileStackParamList } from '../navigation';
-import { useSafeAreaValues } from '@/src/utils';
+import { useSafeAreaValues, DEFAULT_USER_AVATAR } from '@/src/utils';
 
 type TrustListScreenNavigationProp = NativeStackNavigationProp<RootStackParamList>;
 type TrustListScreenRouteProp = {
@@ -417,17 +417,17 @@ export const Trust_TrusterListScreen = () => {
                                 avatars={[
                                     {
                                         id: '1',
-                                        source: require('@/assets/avatar/ozan.png'),
+                                        source: DEFAULT_USER_AVATAR,
                                         alt: 'User 1'
                                     },
                                     {
                                         id: '2',
-                                        source: require('@/assets/avatar/ozan.png'),
+                                        source: DEFAULT_USER_AVATAR,
                                         alt: 'User 2'
                                     },
                                     {
                                         id: '3',
-                                        source: require('@/assets/avatar/ozan.png'),
+                                        source: DEFAULT_USER_AVATAR,
                                         alt: 'User 3'
                                     }
                                 ]}
@@ -482,17 +482,17 @@ export const Trust_TrusterListScreen = () => {
                                 avatars={[
                                     {
                                         id: '1',
-                                        source: require('@/assets/avatar/ozan.png'),
+                                        source: DEFAULT_USER_AVATAR,
                                         alt: 'User 1'
                                     },
                                     {
                                         id: '2',
-                                        source: require('@/assets/avatar/ozan.png'),
+                                        source: DEFAULT_USER_AVATAR,
                                         alt: 'User 2'
                                     },
                                     {
                                         id: '3',
-                                        source: require('@/assets/avatar/ozan.png'),
+                                        source: DEFAULT_USER_AVATAR,
                                         alt: 'User 3'
                                     }
                                 ]}
